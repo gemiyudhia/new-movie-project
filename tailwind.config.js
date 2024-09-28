ec48e8f85b7101f9fc9098fe2ea4ddff63f4e1ab
Gemi <githuba122@gmail.com>
2024-09-28 22:09:08 +0700

    add config in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        background: ["#16161a"],
+        secondary: ["#72757e"],
       },
     },
   },
   plugins: [],
-}
+};
 
