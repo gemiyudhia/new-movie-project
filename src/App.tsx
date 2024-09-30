62441f4ce6a6d2c18f1f68bd5479e4d5f9ee10c7
Gemi <githuba122@gmail.com>
2024-09-30 17:14:22 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import Navbar from "./components/Header";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./pages/Movies";
+import TvSeries from "./pages/TvSeries";
 
 function App() {
   return (
     <>
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/tv" element={<TvSeries />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/TvSeries.tsx
@@ -0,0 +1,9 @@
+const TvSeries = () => {
+  return (
+    <div>
+      <h1>Tv Series pages</h1>
+    </div>
+  )
+}
+
+export default TvSeries
