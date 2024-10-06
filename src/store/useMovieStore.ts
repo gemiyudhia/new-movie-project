1f38f065f07408f58e71aa3762fa1c9077281965
Gemi <githuba122@gmail.com>
2024-10-06 18:35:18 +0700

    feat(store): Add Upcoming and Top Rated movies fetching to Zustand store

--- src/store/useMovieStore.ts
+++ src/store/useMovieStore.ts
@@ -5,13 +5,19 @@ import axios from "axios";
 type movieState = {
   nowPlayingMovies: Movie[];
   popularMovies: Movie[];
+  upComingMovies: Movie[];
+  topRatedMovies: Movie[];
   fetchNowPlayingMovies: (limit?: number) => void;
   fetchPopularMovies: (limit?: number) => void;
+  fetchUpComingMovies: (limit?: number) => void;
+  fetchTopRatedMovies: (limit?: number) => void;
 };
 
 export const useMovieStore = create<movieState>((set) => ({
   nowPlayingMovies: [],
   popularMovies: [],
+  upComingMovies: [],
+  topRatedMovies: [],
 
   fetchNowPlayingMovies: async (limit: number | null = null) => {
     try {
@@ -50,4 +56,43 @@ export const useMovieStore = create<movieState>((set) => ({
       console.log(err);
     }
   },
+
+  fetchUpComingMovies: async (limit: number | null = null) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_REACT_BASE_URL}/movie/upcoming?api_key=${
+          import.meta.env.VITE_REACT_API_KEY
+        }`
+      );
+
+      const data = response.data.results;
+      const slicedData = limit ? data.slice(0, limit) : data;
+
+      set({
+        upComingMovies: slicedData,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  fetchTopRatedMovies: async (limit: number | null = null) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_REACT_BASE_URL}/movie/top_rated?api_key=${
+          import.meta.env.VITE_REACT_API_KEY
+        }`
+      );
+
+      const data = response.data.results;
+      const slicedData = limit ? data.slice(0, limit) : data;
+
+      set({
+        topRatedMovies: slicedData,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
 }));
+
