c434533b37e0037c6c9860d5e75d6eb550b1e547
Gemi <githuba122@gmail.com>
2024-10-06 18:34:26 +0700

    feat(store): Add On The Air and Top Rated series fetching to Zustand store

--- src/store/useSeriesStore.ts
+++ src/store/useSeriesStore.ts
@@ -5,13 +5,19 @@ import axios from "axios";
 type tvSeriesState = {
   popularSeries: TvSeries[];
   airingTodaySeries: TvSeries[];
+  onTheAirSeries: TvSeries[];
+  topRatedSeries: TvSeries[];
   fetchPopularSeries: (limit?: number) => void;
-  // fetchAiringTodaySeries: (limit?: number) => void;
+  fetchAiringTodaySeries: (limit?: number) => void;
+  fetchOnTheAirSeries: (limit?: number) => void;
+  fetchTopRatedSeries: (limit?: number) => void;
 };
 
 export const useSeriesStore = create<tvSeriesState>((set) => ({
   popularSeries: [],
   airingTodaySeries: [],
+  onTheAirSeries: [],
+  topRatedSeries: [],
 
   fetchPopularSeries: async (limit: number | null = null) => {
     try {
@@ -33,23 +39,63 @@ export const useSeriesStore = create<tvSeriesState>((set) => ({
     }
   },
 
-  // fetchAiringTodaySeries: async (limit: number | null = null) => {
-  //   try {
-  //     const response = await axios.get(
-  //       `${import.meta.env.VITE_REACT_BASE_URL}/tv/airing_today?api_key=${
-  //         import.meta.env.VITE_REACT_API_KEY
-  //       }`
-  //     );
+  fetchAiringTodaySeries: async (limit: number | null = null) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_REACT_BASE_URL}/tv/airing_today?api_key=${
+          import.meta.env.VITE_REACT_API_KEY
+        }`
+      );
 
-  //     const data = response.data.results;
+      const data = response.data.results;
 
-  //     const slicedData = limit ? data.slice(0, limit) : data;
+      const slicedData = limit ? data.slice(0, limit) : data;
 
-  //     set({
-  //       popularSeries: slicedData,
-  //     });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // },
+      set({
+        airingTodaySeries: slicedData,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  fetchOnTheAirSeries: async (limit: number | null = null) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_REACT_BASE_URL}/tv/on_the_air?api_key=${
+          import.meta.env.VITE_REACT_API_KEY
+        }`
+      );
+
+      const data = response.data.results;
+
+      const slicedData = limit ? data.slice(0, limit) : data;
+
+      set({
+        onTheAirSeries: slicedData,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  fetchTopRatedSeries: async (limit: number | null = null) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_REACT_BASE_URL}/tv/top_rated?api_key=${
+          import.meta.env.VITE_REACT_API_KEY
+        }`
+      );
+
+      const data = response.data.results;
+
+      const slicedData = limit ? data.slice(0, limit) : data;
+
+      set({
+        topRatedSeries: slicedData,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
 }));
