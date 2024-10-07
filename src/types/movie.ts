e69f6ae94ae7c6f5fda4bab213bdc19d529ae51a
Gemi <githuba122@gmail.com>
2024-10-07 15:36:15 +0700

    delete files as they will be merged

--- src/types/movie.ts
+++
@@ -1,9 +0,0 @@
-export type Movie = {
-  id: number;
-  title: string;
-  overview: string;
-  release_date: string;
-  rating: number;
-  poster_path: string;
-  backdrop_path: string;
-};
--- src/types/tvSeries.ts
+++
@@ -1,9 +0,0 @@
-export type TvSeries ={
-  id: number,
-  name: string,
-  overview: string,
-  poster_path: string,
-  vote_average: number,
-  first_air_date: string,
-  backdrop_path: string,
-}
